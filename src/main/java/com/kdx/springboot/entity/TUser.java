6b4d637bbda1ba9cbe1396e64d136aaa4395ef01
kedongxing <1120901868@qq.com>
2019-06-05 17:38:11 +0800

    Initial commit

---
+++ src/main/java/com/kdx/springboot/entity/TUser.java
@@ -0,0 +1,43 @@
+package com.kdx.springboot.entity;
+
+public class TUser {
+    private Integer id;
+
+    private String name;
+
+    private String realname;
+
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname == null ? null : realname.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+}
